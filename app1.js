7c39aebb780f08d9fa647b08c9ab44c167bf666b
Biswajit-acadgild <biswajit.146@gmail.com>
2016-11-09 18:22:16 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,12 @@
+var personObj = {
+    
+    name : "Biswajit Sahu",
+    age : 23,
+    dob : "22-06-1993",
+    placeOfBirth : "Cuttack"
+}
+
+console.log("Name :"+personObj.name);
+console.log("Age :"+personObj.age);
+console.log("Date of Birth :"+personObj.dob);
+console.log("Place of Birth :"+personObj.placeOfBirth);
